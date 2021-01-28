580fcda5aaec866a0495ac33b91f1c988dec65f4
James Sinkala <xinnks@gmail.com>
2021-01-28 23:09:39 +0300

    improvement(main.rs): Update game info

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use rand::Rng;
 fn main(){
     println!("'Guess The Number' Game!");
     
-    println!("In this game you'll be guessing the secret number between two excluded values. To start choose the lower and higher limits");
+    println!("In this game you'll be guessing the secret number between a range of numbers, the lower being inclusive while the higher being exclusive. To start provide the lower and higher limits of the range of numbers.");
 
     let mut lower_limit = String::new();
     
